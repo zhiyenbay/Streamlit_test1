b6fd23d8065b3ba82d9584903cc8a3d8c456587c
zhiyenbay <zh_olzhas@mail.ru>
2024-03-15 18:00:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,41 +10,38 @@ from streamlit_bokeh_events import streamlit_bokeh_events
 from bokeh.io import show
 
 stt_button = Button(label="Speak", width=100)
-button = Button(label = "Click on the button", 
-                button_type = "danger") 
-show(button)
 show(stt_button)
 
-# stt_button.js_on_event("button_click", CustomJS(code="""
-#     var recognition = new webkitSpeechRecognition();
-#     recognition.continuous = true;
-#     recognition.interimResults = true;
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
  
-#     recognition.onresult = function (e) {
-#         var value = "";
-#         for (var i = e.resultIndex; i < e.results.length; ++i) {
-#             if (e.results[i].isFinal) {
-#                 value += e.results[i][0].transcript;
-#             }
-#         }
-#         if ( value != "") {
-#             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-#         }
-#     }
-#     recognition.start();
-#     """))
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
 
-# result = streamlit_bokeh_events(
-#     stt_button,
-#     events="GET_TEXT",
-#     key="listen",
-#     refresh_on_update=False,
-#     override_height=75,
-#     debounce_time=0)
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-# if result:
-#     if "GET_TEXT" in result:
-#         st.write(result.get("GET_TEXT"))
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
 
 schema = {
     "$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -67,40 +64,40 @@ schema = {
 }
 
 
-# st.title('Quickstart App')
+st.title('Quickstart App')
 
-# openai_api_key = st.sidebar.text_input('OpenAI API Key')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-# # def generate_response(input_text):
-# #   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-# #   st.info(llm(input_text))
+# def generate_response(input_text):
+#   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+#   st.info(llm(input_text))
 
-# with st.form('my_form'):
-#   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-#   prompt = "Map information to a valid  JSON output according to the provided JSON Schema. Information: " + text
-#   submitted = st.form_submit_button('Submit')
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  prompt = "Map information to a valid  JSON output according to the provided JSON Schema. Information: " + text
+  submitted = st.form_submit_button('Submit')
   
-#   if not openai_api_key.startswith('sk-'):
-#     st.warning('Please enter your OpenAI API key!', icon='⚠')
-#   if submitted and openai_api_key.startswith('sk-'):
-#     client = OpenAI(
-#       api_key = openai_api_key
-#     )
-#     chat_completion = client.chat.completions.create(
-#     model="gpt-3.5-turbo-1106",
-#     response_format={"type":"json_object"},
-#     messages=[
-#         {"role":"system","content":"Answer according to following Json Schema: "+ json.dumps(schema)},
-#         {"role":"user","content":prompt}
-#     ],
-#     temperature = 0
-#     )
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    client = OpenAI(
+      api_key = openai_api_key
+    )
+    chat_completion = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    response_format={"type":"json_object"},
+    messages=[
+        {"role":"system","content":"Answer according to following Json Schema: "+ json.dumps(schema)},
+        {"role":"user","content":prompt}
+    ],
+    temperature = 0
+    )
 
-#     finish_reason = chat_completion.choices[0].finish_reason
+    finish_reason = chat_completion.choices[0].finish_reason
 
-#     if(finish_reason == "stop"):
-#         data = chat_completion.choices[0].message.content
-#         st.info(data)
+    if(finish_reason == "stop"):
+        data = chat_completion.choices[0].message.content
+        st.info(data)
 
-#     else :
-#         st.info("Error! provide more tokens please")
+    else :
+        st.info("Error! provide more tokens please")
