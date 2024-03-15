4b985e560d61e87dab7b5ea9337f9f84ed283aa6
zhiyenbay <zh_olzhas@mail.ru>
2024-03-15 18:20:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,38 +41,38 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key')
 #   st.info(llm(input_text))
 
 with st.form('my_form'):
-    stt_button = Button(label="Speak", width=100)
+    # stt_button = Button(label="Speak", width=100)
 
-    stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
+    # stt_button.js_on_event("button_click", CustomJS(code="""
+    # var recognition = new webkitSpeechRecognition();
+    # recognition.continuous = true;
+    # recognition.interimResults = true;
  
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
+    # recognition.onresult = function (e) {
+    #     var value = "";
+    #     for (var i = e.resultIndex; i < e.results.length; ++i) {
+    #         if (e.results[i].isFinal) {
+    #             value += e.results[i][0].transcript;
+    #         }
+    #     }
+    #     if ( value != "") {
+    #         document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+    #     }
+    # }
+    # recognition.start();
+    # """))
 
-    result = streamlit_bokeh_events(
-        stt_button,
-        events="GET_TEXT",
-        key="listen",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0)
+    # result = streamlit_bokeh_events(
+    #     stt_button,
+    #     events="GET_TEXT",
+    #     key="listen",
+    #     refresh_on_update=False,
+    #     override_height=75,
+    #     debounce_time=0)
     
-    if result:
-        if "GET_TEXT" in result:
-            st.write(result.get("GET_TEXT"))
+    # if result:
+    #     if "GET_TEXT" in result:
+    #         st.write(result.get("GET_TEXT"))
     
 
 
