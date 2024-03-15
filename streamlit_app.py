cae36ce83eb9f032578d1b651614fb51dbe81390
zhiyenbay <zh_olzhas@mail.ru>
2024-03-15 17:24:32 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from langchain.llms import OpenAI
 from openai import OpenAI
 import json
 
-from bokeh.models.widgets import Button
+from bokeh.models import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
