d1c46c4f0c3ebbc7685ed91119ee9ffa947f0bee
zhiyenbay <zh_olzhas@mail.ru>
2024-03-15 16:51:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,40 @@ import json
 
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-text = st.text_input("Say what ?")
+stt_button = Button(label="Speak", width=100)
 
-tts_button = Button(label="Speak", width=100)
-
-tts_button.js_on_event("button_click", CustomJS(code=f"""
-    var u = new SpeechSynthesisUtterance();
-    u.text = "{text}";
-    u.lang = 'en-US';
-
-    speechSynthesis.speak(u);
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
     """))
 
-st.bokeh_chart(tts_button)
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
 
 schema = {
     "$schema": "https://json-schema.org/draft/2020-12/schema",
